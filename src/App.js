7b3737ab1eb17af6b5580df161387ad34647c940
semir709 <selmansemir555@gmail.com>
2024-09-30 12:46:13 +0200

    created the about page

--- src/App.js
+++ src/App.js
@@ -1,63 +1,53 @@
+import "./App.css";
 
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import Main from "./pages/main/Main";
+import Game from "./pages/game/Game";
+import GameSS from "./pages/game_screenshots/GameSS";
+import Navbar from "./components/navbar/Navbar";
+import About from "./pages/about/About";
+import Sidenav from "./components/sidenav/Sidenav";
+import { useState } from "react";
 
-import Main from './pages/main/Main'
-import Game from './pages/game/Game';
-import GameSS from './pages/game_screenshots/GameSS';
-import Navbar from './components/navbar/Navbar';
-import Sidenav from './components/sidenav/Sidenav';
-import { useState } from 'react';
-
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 function App() {
   // let [query, setQuery] = useState('all');
 
-  const [newSelected, setNewSelected] = useState('All Games');
+  const [newSelected, setNewSelected] = useState("All Games");
 
   return (
     <div className="App">
-      <div id='overlay'></div>
+      <div id="overlay"></div>
 
       <Router>
-
         <Navbar setNewSelected={setNewSelected} />
 
-        <div className='d-flex justify-content-between'>
-
-          <div className='sidenav-wrapper'>
+        <div className="d-flex justify-content-between">
+          <div className="sidenav-wrapper">
             <Sidenav setNewSelected={setNewSelected} />
           </div>
 
-
           <QueryClientProvider client={queryClient}>
             <Routes>
-
-              <Route path='/:category' element={<Main text={newSelected} />} />
-              <Route path='/:category/:filter' element={<Main text={newSelected} />} />
-              <Route path='/' element={<Main text={newSelected} />} />
-              <Route path='/:filter' element={<Main text={newSelected} />} />
+              <Route path="/:category" element={<Main text={newSelected} />} />
+              <Route
+                path="/:category/:filter"
+                element={<Main text={newSelected} />}
+              />
+              <Route path="/" element={<Main text={newSelected} />} />
+              <Route path="/:filter" element={<Main text={newSelected} />} />
               {/* <Route path='/home' element={<Main query={query} />} /> */}
-              <Route path='/game/:id' element={<Game />} />
-              <Route path='/game/:id/screenshots' element={<GameSS />} />
-
+              <Route path="/game/:id" element={<Game />} />
+              <Route path="/game/:id/screenshots" element={<GameSS />} />
+              <Route path="/about" element={<About />} />
             </Routes>
-
           </QueryClientProvider>
-
         </div>
-
       </Router>
-
     </div>
   );
 }
---
+++ src/pages/about/About.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import "./about.css";
+
+const About = () => {
+  return (
+    <div className="container">
+      <h1>About</h1>
+      <div className="content">
+        <p>
+          <span>
+            Welcome to my project, a clone of the RAWG video game database
+            website! This site was created as a personal challenge to improve my
+            front-end development skills, with a focus on implementing infinite
+            loading for content. I use the RAWG API to provide all the game data
+            displayed here. Special thanks to RAWG for making their API
+            available. Please note that this site is for educational purposes
+            only and is not intended for commercial use.
+          </span>
+        </p>
+        <p className="contact">
+          <span>
+            If you'd like to follow my work or get in touch, feel free to reach
+            out: <br />
+            GitHub:
+            <a
+              href="https://github.com/semir709"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://github.com/semir709
+            </a>
+            <br /> Username Email: selmansemir5@gmail.com <br />
+            I’m always open to feedback, collaboration, and learning
+            opportunities!
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
