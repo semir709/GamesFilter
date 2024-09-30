a8ef6801218cd60c83a803df7d9f90f6a8340411
semir709 <selmansemir555@gmail.com>
2024-09-30 14:48:23 +0200

    fix loading page messages

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,175 +1,177 @@
+import { faTruckLoading } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { useRef } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import svg from "../../utils/svg";
+import Sidenav from "../sidenav/Sidenav";
 
-import { faTruckLoading } from '@fortawesome/free-solid-svg-icons';
-import axios from 'axios';
-import { useRef } from 'react';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import svg from '../../utils/svg';
-import Sidenav from '../sidenav/Sidenav';
-
-import './navbar.css';
+import "./navbar.css";
 
 const Navbar = ({ setNewSelected }) => {
+  const [searchMenu, setSearchMenu] = useState(false);
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState(null);
+  const inputRef = useRef(null);
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [showNavBar, setShowNavBar] = useState(true);
 
-    const [searchMenu, setSearchMenu] = useState(false);
-    const [search, setSearch] = useState('');
-    const [data, setData] = useState(null);
-    const inputRef = useRef(null);
-    const [toggleSidebar, setToggleSidebar] = useState(false);
-    const [showNavBar, setShowNavBar] = useState(true);
+  const click_menu_btn = (e) => {
+    // const menu_items = document.querySelector('#mobile-side-content');
+    // menu_items.classList.add('show-menu-items');
+    setToggleSidebar(true);
+    const overlay = document.querySelector("#overlay");
 
-    const click_menu_btn = (e) => {
-        // const menu_items = document.querySelector('#mobile-side-content');
-        // menu_items.classList.add('show-menu-items');
-        setToggleSidebar(true);
-        const overlay = document.querySelector('#overlay');
+    overlay.classList.add("show-overlay");
+    document.body.classList.add("stop-scrolling");
+  };
 
-        overlay.classList.add('show-overlay');
-        document.body.classList.add('stop-scrolling');
+  const click_close_btn = (e) => {
+    setToggleSidebar(false);
+    // const menu_items = document.querySelector('#mobile-side-content');
+    // menu_items.classList.remove('show-menu-items');
+    const overlay = document.querySelector("#overlay");
+
+    overlay.classList.remove("show-overlay");
+    document.body.classList.remove("stop-scrolling");
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+
+    if (e.target.value === "") setSearchMenu(false);
+  };
+
+  let cancel;
+  useEffect(() => {
+    axios
+      .get(`https://api.rawg.io/api/games`, {
+        method: "GET",
+        params: {
+          key: process.env.REACT_APP_KEY,
+          search: search,
+        },
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setData(res.data.results);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+      });
+
+    return () => cancel();
+  }, [search]);
+
+  useEffect(() => {
+    if (
+      document.activeElement === inputRef.current &&
+      inputRef.current.value.length > 0
+    ) {
+      setSearchMenu(true);
+    }
+  }, [data]);
+
+  window.onclick = (e) => {
+    if (e.target !== inputRef.current) {
+      setSearchMenu(false);
+      inputRef.current.value = "";
+    }
+  };
+
+  const sideClick = () => {
+    const overlay = document.querySelector("#overlay");
+    overlay.classList.remove("show-overlay");
+    document.body.classList.remove("stop-scrolling");
+    setToggleSidebar(false);
+  };
+
+  let oldScrollY = 0;
+  const contorlNavbar = () => {
+    if (window.scrollY > oldScrollY) {
+      setShowNavBar(true);
+    } else if (window.scrollY < oldScrollY && window.scrollY !== 0) {
+      setShowNavBar(false);
+    } else if (window.scrollY < oldScrollY && window.scrollY === 0) {
+      setShowNavBar(true);
     }
 
-    const click_close_btn = (e) => {
-        setToggleSidebar(false);
-        // const menu_items = document.querySelector('#mobile-side-content');
-        // menu_items.classList.remove('show-menu-items');
-        const overlay = document.querySelector('#overlay');
+    oldScrollY = window.scrollY;
+  };
 
-        overlay.classList.remove('show-overlay');
-        document.body.classList.remove('stop-scrolling');
-    }
+  useEffect(() => {
+    window.addEventListener("scroll", contorlNavbar);
 
-    const handleSearch = (e) => {
-        setSearch(e.target.value);
+    return () => {
+      window.removeEventListener("scroll", contorlNavbar);
+    };
+  }, []);
 
-        if (e.target.value === '') setSearchMenu(false);
-    }
+  return (
+    <header
+      className={`${
+        showNavBar && "navbar-holder-show "
+      }  navbar-holder d-flex justify-content-between align-items-center mt-3 px-3`}
+    >
+      <Link to={"/"} onClick={() => setNewSelected("All Games")}>
+        Logo
+      </Link>
 
-    let cancel;
-    useEffect(() => {
-        axios.get(`https://api.rawg.io/api/games`, {
-            method: 'GET',
-            params: {
-                key: process.env.REACT_APP_KEY,
-                search: search
-            },
-            cancelToken: new axios.CancelToken(c => cancel = c)
-        }).then(res => {
-            setData(res.data.results);
-        }).catch(e => {
-            if (axios.isCancel(e)) return
-        });
+      <div className="search-wrapper">
+        <div className="input-group d-flex align-items-center ">
+          <label htmlFor="search-input" className="label-search">
+            <img src={svg.search} alt="search" />
+          </label>
 
-        return () => cancel()
-    }, [search]);
+          <input
+            id="search-input"
+            ref={inputRef}
+            onChange={handleSearch}
+            type="text"
+            placeholder="Search..."
+          />
+        </div>
 
-    useEffect(() => {
+        <div className="search-res-wrapper">
+          {searchMenu &&
+            data.map(({ name, id, background_image }) => {
+              return (
+                <Link to={`game/${id}`} key={id}>
+                  <div className="res-item">
+                    <img src={background_image} alt="" />
+                    <p>{name}</p>
+                  </div>
+                </Link>
+              );
+            })}
+        </div>
+      </div>
 
-        if (document.activeElement === inputRef.current && inputRef.current.value.length > 0) {
-            setSearchMenu(true);
-        }
+      <Link className="about-text" to={"/about"}>
+        About
+      </Link>
 
-    }, [data]);
+      <div className="mobile-side-content-holder">
+        <div className="mobile-menu-open" onClick={click_menu_btn}>
+          <img src={svg.menu} alt="asd" />
+        </div>
 
-    window.onclick = (e) => {
-        if (e.target !== inputRef.current) {
-            setSearchMenu(false);
-            inputRef.current.value = '';
-        }
-    }
-
-    const sideClick = () => {
-        const overlay = document.querySelector('#overlay');
-        overlay.classList.remove('show-overlay');
-        document.body.classList.remove('stop-scrolling');
-        setToggleSidebar(false);
-    }
-
-    let oldScrollY = 0;
-    const contorlNavbar = () => {
-
-        if (window.scrollY > oldScrollY) {
-            setShowNavBar(true);
-        } else if (window.scrollY < oldScrollY && window.scrollY !== 0) {
-            setShowNavBar(false);
-        } else if (window.scrollY < oldScrollY && window.scrollY === 0) {
-            setShowNavBar(true);
-        }
-
-        oldScrollY = window.scrollY;
-    }
-
-    useEffect(() => {
-        window.addEventListener('scroll', contorlNavbar);
-
-        return () => {
-            window.removeEventListener('scroll', contorlNavbar);
-        }
-    }, []);
-
-    return (
-
-
-        <header className={`${showNavBar && 'navbar-holder-show '}  navbar-holder d-flex justify-content-between align-items-center mt-3 px-3`}>
-
-            <Link to={'/'} onClick={() => setNewSelected('All Games')}>Logo</Link>
-
-            <div className='search-wrapper'>
-
-                <div className='input-group d-flex align-items-center '>
-
-                    <label htmlFor="search-input" className='label-search'>
-                        <img src={svg.search} alt="search" />
-                    </label>
-
-                    <input id='search-input' ref={inputRef} onChange={handleSearch} type="text" placeholder='Search...' />
-
-                </div>
-
-                <div className='search-res-wrapper'>
-
-                    {!data && <div>Loading...</div>}
-
-                    {searchMenu && data.map(({ name, id, background_image }) => {
-                        return <Link to={`game/${id}`} key={id}>
-                            <div className='res-item'>
-                                <img src={background_image} alt="" />
-                                <p>{name}</p>
-                            </div>
-                        </Link>
-
-                    })}
-                </div>
-
+        {toggleSidebar && (
+          <div className="mobile-side-content" id="mobile-side-content">
+            <div className="mobile-menu-close" onClick={click_close_btn}>
+              <img src={svg.close} alt="" />
             </div>
 
-            <Link className='about-text' to={'/about'}>About</Link>
+            <Sidenav onClick={sideClick} />
 
-            <div className='mobile-side-content-holder'>
-                <div className='mobile-menu-open' onClick={click_menu_btn}>
-                    <img src={svg.menu} alt="asd" />
-                </div>
-
-                {toggleSidebar && <div className='mobile-side-content' id='mobile-side-content'>
-
-                    <div className='mobile-menu-close' onClick={click_close_btn}>
-                        <img src={svg.close} alt="" />
-                    </div>
-
-                    <Sidenav onClick={sideClick} />
-
-                    <Link className='about-text' to={'/about'}>About</Link>
-
-                </div>}
-
-            </div>
-
-
-        </header>
-
-
-
-
-    );
-}
+            <Link className="about-text" to={"/about"}>
+              About
+            </Link>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
--- src/pages/game_screenshots/GameSS.js
+++ src/pages/game_screenshots/GameSS.js
@@ -1,140 +1,150 @@
 import Header from "../../components/header/Header";
-import './gamess.css';
+import "./gamess.css";
 import Image from "../../components/image/Image";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 import Modal from "../../components/lightBox/components/Modal";
+import Loading from "../../components/loading/Loading";
 
 const GameSS = () => {
-    const { id } = useParams();
-    const [media, setMedia] = useState(null);
-    const [name, setName] = useState(null);
-    const [clickedImg, setClickedImg] = useState(null);
-    const [currentIndex, setCurrentIndex] = useState(null);
+  const { id } = useParams();
+  const [media, setMedia] = useState(null);
+  const [name, setName] = useState(null);
+  const [clickedImg, setClickedImg] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(null);
 
-    const handleClick = (item, index, e) => {
+  const handleClick = (item, index, e) => {
+    setCurrentIndex(index);
+    setClickedImg(item.image);
+    // setType(item.type);
+  };
 
-        setCurrentIndex(index);
-        setClickedImg(item.image);
-        // setType(item.type); 
-    };
+  const handelRotationLeft = () => {
+    const totalLength = media.length;
+    if (currentIndex === 0) {
+      setCurrentIndex(totalLength - 1);
+      let newUrl;
+      if (media[totalLength - 1].image?.length > 0)
+        newUrl = media[totalLength - 1].image;
+      if (media[totalLength - 1].data?.max.length > 0)
+        newUrl = media[totalLength - 1].data.max;
+      setClickedImg(newUrl);
+      return;
+    }
+    const newIndex = currentIndex - 1;
+    const newUrl = media.filter((item) => {
+      return media.indexOf(item) === newIndex;
+    });
+    let newItem;
+    if (newUrl[0].image?.length > 0) newItem = newUrl[0].image;
+    if (newUrl[0].data?.max.length > 0) newItem = newUrl[0].data.max;
+    const newType = newUrl[0].type;
+    setClickedImg(newItem);
+    setCurrentIndex(newIndex);
+  };
 
+  const handelRotationRight = () => {
+    const totalLength = media.length;
+    if (currentIndex + 1 >= totalLength) {
+      setCurrentIndex(0);
+      // const newUrl = media[0].url;
+      let newUrl;
+      if (media[0].image?.length > 0) newUrl = media[0].image;
+      if (media[0].data?.max.length > 0) newUrl = media[0].data.max;
+      setClickedImg(newUrl);
+      return;
+    }
+    const newIndex = currentIndex + 1;
+    const newUrl = media.filter((item) => {
+      return media.indexOf(item) === newIndex;
+    });
+    // const newItem = newUrl[0].url;
+    let newItem;
+    if (newUrl[0].image?.length > 0) newItem = newUrl[0].image;
+    if (newUrl[0].data?.max.length > 0) newItem = newUrl[0].data.max;
+    const newType = newUrl[0].type;
+    setClickedImg(newItem);
+    setCurrentIndex(newIndex);
+  };
 
-    const handelRotationLeft = () => {
-        const totalLength = media.length;
-        if (currentIndex === 0) {
-            setCurrentIndex(totalLength - 1);
-            let newUrl;
-            if (media[totalLength - 1].image?.length > 0) newUrl = media[totalLength - 1].image;
-            if (media[totalLength - 1].data?.max.length > 0) newUrl = media[totalLength - 1].data.max;
-            setClickedImg(newUrl);
-            return;
-        }
-        const newIndex = currentIndex - 1;
-        const newUrl = media.filter((item) => {
-            return media.indexOf(item) === newIndex;
-        });
-        let newItem;
-        if (newUrl[0].image?.length > 0) newItem = newUrl[0].image;
-        if (newUrl[0].data?.max.length > 0) newItem = newUrl[0].data.max;
-        const newType = newUrl[0].type;
-        setClickedImg(newItem);
-        setCurrentIndex(newIndex);
-    };
+  const endpoints = [
+    `https://api.rawg.io/api/games/${id}`,
+    `https://api.rawg.io/api/games/${id}/screenshots`,
+    `https://api.rawg.io/api/games/${id}/movies`,
+  ];
 
-    const handelRotationRight = () => {
-        const totalLength = media.length;
-        if (currentIndex + 1 >= totalLength) {
-            setCurrentIndex(0);
-            // const newUrl = media[0].url;
-            let newUrl;
-            if (media[0].image?.length > 0) newUrl = media[0].image;
-            if (media[0].data?.max.length > 0) newUrl = media[0].data.max;
-            setClickedImg(newUrl);
-            return;
-        }
-        const newIndex = currentIndex + 1;
-        const newUrl = media.filter((item) => {
-            return media.indexOf(item) === newIndex;
-        });
-        // const newItem = newUrl[0].url;
-        let newItem;
-        if (newUrl[0].image?.length > 0) newItem = newUrl[0].image;
-        if (newUrl[0].data?.max.length > 0) newItem = newUrl[0].data.max;
-        const newType = newUrl[0].type;
-        setClickedImg(newItem);
-        setCurrentIndex(newIndex);
-    };
+  useEffect(() => {
+    axios
+      .all(
+        endpoints.map((endpoint) =>
+          axios.get(endpoint, {
+            method: "GET",
+            params: { key: process.env.REACT_APP_KEY },
+          })
+        )
+      )
+      .then((res) => {
+        setName(res[0].data.name);
+        const m = res[2].data.results.concat(res[1].data.results);
+        setMedia(m);
+      });
+  }, [id]);
 
-    const endpoints = [
-        `https://api.rawg.io/api/games/${id}`,
-        `https://api.rawg.io/api/games/${id}/screenshots`,
-        `https://api.rawg.io/api/games/${id}/movies`
-    ]
+  // if (!media) return <p>Loading...</p>
 
-    useEffect(() => {
-        axios.all(
-            endpoints.map((endpoint) =>
-                axios.get(
-                    endpoint,
-                    {
-                        method: 'GET',
-                        params: { key: process.env.REACT_APP_KEY }
-                    }
-                ))).then(res => {
-                    setName(res[0].data.name);
-                    const m = res[2].data.results.concat(res[1].data.results);
-                    setMedia(m);
-                });
-    }, [id]);
-
-
-    if (!media) return <p>Loading...</p>
-
-
+  if (!media)
     return (
-
-        <div className="game-ss-holder ">
-            <Header text={name} />
-
-            <div className="disc-text mt-5">
-                <p>Screen shots</p>
-            </div>
-
-            <div className="d-flex mb-5 game-ss-wrapper">
-
-                {media.map((prop, index) => {
-
-                    if (prop.data?.max.length > 0) {
-                        return <div key={prop.id} className="content-wrapper">
-                            <video src={prop.data.max} controls poster={prop.preview}></video>
-                        </div>
-                    } else if (prop.image?.length > 0) {
-                        return <div key={index} className="content-wrapper">
-                            <img src={prop.image} onClick={(e) => handleClick(prop, index, e)}></img>
-                        </div>
-                    }
-                })}
-
-            </div>
-
-
-            {clickedImg && (
-                <Modal
-                    clickedImg={clickedImg}
-                    handelRotationRight={handelRotationRight}
-                    setClickedImg={setClickedImg}
-                    handelRotationLeft={handelRotationLeft}
-                // type={type}
-                />
-            )}
-
-        </div>
-
+      <div className="setCenter">
+        <Loading fontSize={40} />
+      </div>
     );
 
-}
+  return (
+    <div className="game-ss-holder ">
+      <Header text={name} />
+
+      <div className="disc-text mt-5">
+        <p>Screen shots</p>
+      </div>
+
+      <div className="d-flex mb-5 game-ss-wrapper">
+        {media.map((prop, index) => {
+          if (prop.data?.max.length > 0) {
+            return (
+              <div key={prop.id} className="content-wrapper">
+                <video
+                  src={prop.data.max}
+                  controls
+                  poster={prop.preview}
+                ></video>
+              </div>
+            );
+          } else if (prop.image?.length > 0) {
+            return (
+              <div key={index} className="content-wrapper">
+                <img
+                  src={prop.image}
+                  onClick={(e) => handleClick(prop, index, e)}
+                ></img>
+              </div>
+            );
+          }
+        })}
+      </div>
+
+      {clickedImg && (
+        <Modal
+          clickedImg={clickedImg}
+          handelRotationRight={handelRotationRight}
+          setClickedImg={setClickedImg}
+          handelRotationLeft={handelRotationLeft}
+          // type={type}
+        />
+      )}
+    </div>
+  );
+};
 
 export default GameSS;
